afa7452fe9351a9707339980ae5cbffb25aea19a
catherineair <113204242+catherineair@users.noreply.github.com>
2023-01-15 01:45:07 +0200

    hmw31-32

--- app.js
+++ app.js
@@ -1,30 +1,12 @@
+
 "use strict";
-const minCode = "a".charCodeAt(0);
-const maxCode = "z".charCodeAt(0);
-function shiftCipher(msg, shift = 1) {
-    return msgProcessing(msg, shift);
-}
-function shiftDecipher(msg, shift = 1) {
-    return msgProcessing(msg, -shift);
-}
-function msgProcessing(msg, shift) {
-    shift = shift % (maxCode - minCode);
-    const arrayOfSymbols = msg.split('');
-    arrayOfSymbols.forEach((symbol, ind, array) => {
-        symbolProcessing(array, ind, symbol.charCodeAt(0), shift);
-    });
-    return arrayOfSymbols.join('');
-}
-function symbolProcessing(arrayOfSymbols, ind, code, shift) {
-    if (code >= minCode && code <= maxCode) {
-        code = code + shift;
-        if (code > maxCode) {
-            code = minCode + (code - maxCode) - 1;
-        }
-        else if (code < minCode) {
-            code = maxCode - (minCode - code) + 1;
-        }
-        arrayOfSymbols[ind] = String.fromCharCode(code);
-    }
-}
+Object.defineProperty(exports, "__esModule", { value: true });
+const Rectangle_1 = require("./Rectangle");
+const Square_1 = require("./Square");
+const shapes = [
+    new Rectangle_1.Rectangle(3, 4),
+    new Square_1.Square(5)
+];
+const sumOfShapes = shapes.reduce((res, cur) => res + cur.getSquare(), 0);
+console.log(sumOfShapes);
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,36 +1,9 @@
-const minCode: number = "a".charCodeAt(0);
-const maxCode: number = "z".charCodeAt(0);
-
-function shiftCipher(msg: string, shift: number = 1): string 
-{
-  return msgProcessing(msg, shift);
-}
-
-function shiftDecipher(msg: string, shift: number = 1): string 
-{
-    return msgProcessing(msg, -shift);
-}
-
-function msgProcessing(msg: string, shift: number): string 
-{
-    shift = shift % (maxCode-minCode);
-    const arrayOfSymbols = msg.split('');
-    arrayOfSymbols.forEach( (symbol: string, ind:number, array: string[])=> {
-        symbolProcessing(array, ind, symbol.charCodeAt(0), shift);
-    })
-    return arrayOfSymbols.join('');
-}
-
-function symbolProcessing(arrayOfSymbols: string[], ind: number, code: number, shift:number): void {
-    if(code >= minCode && code <= maxCode) 
-    {
-        code = code + shift;
-        if(code > maxCode) {
-            code = minCode + (code - maxCode) - 1;
-        } else if(code < minCode) {
-            code = maxCode - (minCode - code) + 1;
-        }
-        arrayOfSymbols[ind] = String.fromCharCode(code);
-    }
-}
-
+import { Shape } from "./ShapeInterface";
+import { Rectangle } from "./Rectangle";
+import { Square } from "./Square";
+const shapes: Array<Shape> = [
+    new Rectangle(3, 4),
+    new Square(5)
+];
+const sumOfShapes: number = shapes.reduce((res, cur) => res + cur.getSquare(), 0)
+console.log(sumOfShapes)
